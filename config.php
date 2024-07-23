7963abb53a34e3fe11f8c0829e00697011279599
Alisher <adm.erikov@gmail.com>
2024-07-24 01:49:49 +0500

    first commit

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://gamer2/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://gamer2/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OSPanel/domains/gamer2/catalog/');
+define('DIR_SYSTEM', 'C:/OSPanel/domains/gamer2/system/');
+define('DIR_IMAGE', 'C:/OSPanel/domains/gamer2/image/');
+define('DIR_STORAGE', 'C:/OSPanel/domains/gamer2/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'gamer2');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
